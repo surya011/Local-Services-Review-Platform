bac1c15a1ed1707be6f31352408f5de2ea82aa9b
prashant7122 <007yadavprashant@gmail.com>
2023-10-28 16:35:48 +0530

    Initial Project Setup

---
+++ ratingManagementService/src/test/java/dev/localservicereview/ratingmanagementservice/RatingManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.localservicereview.ratingmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
